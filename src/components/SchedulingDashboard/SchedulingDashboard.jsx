6a9d2e65ead47936606ac4caa8f1e00f62e9b0af
Conor Callahan <conorcallahan97@outlook.com>
2024-09-02 17:01:09 -0500

    Had a misnamed file for the css import on schedulingdashboard.jsx but now its correct.

--- src/components/SchedulingDashboard/SchedulingDashboard.jsx
+++ src/components/SchedulingDashboard/SchedulingDashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import moment from 'moment';
-import './SchedulingDashboard.css';
+import './SchedulingDashBoard.css';
 
 function SchedulingDashboard() {
   const dispatch = useDispatch();
